9ee351bc5f47db06221281fb53cc6edd9cc8cf8e
andreadgobbo@gmail.com <andreadgobbo@gmail.com>
2020-11-08 20:54:05 +0100

    Aggiungere i file di progetto.

---
+++ libreriamatematica/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace libreriamatematica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int somma = libreriamatematica.somma(4,5);
+            Console.WriteLine($"la somma {somma}");
+            double sommad = libreriamatematica.sommad(4.5, 5.4);
+            Console.WriteLine($"la somam {sommad}");
+            double sottrazione = libreriamatematica.sottrazione(4, 5);
+            Console.WriteLine($"la sottrazione {sottrazione}");
+            long potenza = libreriamatematica.potenza(3, 3);
+            Console.WriteLine($"la potenza {potenza}");
+            double divisione = libreriamatematica.divisione(5, 4);
+            Console.WriteLine($"la divisione {divisione}");
+            double moltiplicazione = libreriamatematica.moltiplicazione(4, 5);
+            Console.WriteLine($"la moltiplicazione {moltiplicazione}");
+            int minore = libreriamatematica.minore(4, 6, 8);
+            Console.WriteLine($"il minore {minore}");
+            int maggiore = libreriamatematica.maggiore(4, 6, 8);
+            Console.WriteLine($"il maggiore {maggiore}");
+            int inverso = libreriamatematica.inverso(4, 5, 7);
+            Console.WriteLine($"il numero inverso {inverso}");
+            int pari = libreriamatematica.pari(6, 5, 2);
+            Console.WriteLine($"il numero pari {pari}");
+            int dispari = libreriamatematica.dispari(5, 8, 6);
+            Console.WriteLine($"il numero dispari {dispari}");
+            int positivo = libreriamatematica.positivo(6, 4, -7);
+            Console.WriteLine($"il numero positivo {positivo}");
+            int negativo = libreriamatematica.negativo(7, -9, -7);
+            Console.WriteLine($"il numero è negativo {negativo}");
+            int intero = libreriamatematica.intero(4.4, 5, 8.6);
+            Console.WriteLine($"il numero intero {intero}");
+
+
+
+        }
+    }
+}
---
+++ libreriamatematica/libreriamatematica.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Text;
+
+namespace libreriamatematica
+{
+    class libreriamatematica
+    {
+        public static int somma(int a,int b)
+        {
+            int somma = a + b;
+            return somma;
+        }
+        public static double sommad(double a , double b)
+        {
+            double somma = a + b;
+            return somma;
+        }
+        public static double sottrazione(double a,double b)
+        {
+            double sottrazione = a - b;
+            return sottrazione;
+        }
+        public static double moltiplicazione(double a,double b)
+        {
+            double moltiplicazione = a * b;
+            return moltiplicazione;
+        }
+        public static double divisione(double a,double b)
+        {
+            double divisione = a / b;
+            return divisione;
+        }
+        public static long potenza(int a, int b)
+        {
+            long potenza = a ^ a;
+            return potenza;
+        }
+        public static int minore(int a, int b, int c)
+        {
+           int minore= Math.Min(a, b);Math.Min(minore, c);
+            return minore;
+        }
+
+        internal static int inverso(int v1, int v2, int v3)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static int pari(int v1, int v2, int v3)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static int dispari(int v1, int v2, int v3)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static int positivo(int v1, int v2, int v3)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static int negativo(int v1, int v2, int v3)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static int intero(double v1, int v2, double v3)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static int maggiore(int a, int b, int c)
+        {
+            int maggiore = Math.Max(a, b);Math.Max(maggiore, c);
+            return maggiore;
+        }
+        public static int inverso(int a)
+        {
+            int iverso = a * -1;
+            return iverso;
+        }
+        public static int pari(int a, int v)
+        {
+            return (a % 2);
+        }
+        public static int dispari (int a, int v)
+        {
+            return (a % 3);
+        }
+        public static bool positivo (int a)
+        {
+            return  a > 0;
+        }
+        public static bool negativo (int a)
+        {
+            return a < 0;
+        }
+        public static bool intero(double a)
+        {
+            return a == (int) a;
+        }
+
+    }
+}
